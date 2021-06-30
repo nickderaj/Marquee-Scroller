415f5d5e0edc34985f2481d09afc0afc19d78fe2
Nick De Raj <nickderaj@gmail.com>
2021-06-30 21:39:03 +0800

    Refactoring code to use JS instead of calculating through CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector(".marquee-content");
