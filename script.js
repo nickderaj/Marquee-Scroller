a92743f51377c3e0adefdb9e3ef6adfd1887e3b1
Nick De Raj <nickderaj@gmail.com>
2021-06-30 21:42:22 +0800

    Using JS to duplicate elements in HTML automatically to make the loop appear infinitely

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
   "--marquee-elements-displayed"
 );
 const marqueeContent = document.querySelector(".marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
